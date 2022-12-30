1f1c8bba0ffa176549992a4b52529440e81dbbd2
Youmbi Leo <Leoyoumbi83@gmail.com>
2022-12-30 17:03:38 +0100

    Update 41.py

--- 41.py
+++ 41.py
@@ -4,8 +4,6 @@ from math import sqrt
 def disbtwpoints(x1, x2, y1, y2):
     return sqrt(((x1-x2)**2)+((y1-y2)**2))
 
-x_1 = eval(input("Enter x1: "))
-x_2 = eval(input("Enter x2: "))
-y_1 = eval(input("Enter y1: "))
-y_2 = eval(input("Enter y2: "))
+x_1, y_1 = [eval(i) for i in input("Enter the x and y coordinates of the first point: ").split()]
+x_2, y_2 = [eval(i) for i in input("Enter the x and y coordinates of the second point: ").split()]
 print(f"The distance between ({x_1},{y_1}) and ({x_2},{y_2}) is {disbtwpoints(x_1, x_2, y_1, y_2)}")
