17ff69f7a92bb42f2fd878b80e15e596f211fa74
Youmbi Leo <Leoyoumbi83@gmail.com>
2022-12-30 01:26:48 +0100

    Update 34.py

--- 34.py
+++ 34.py
@@ -1,12 +1,5 @@
-"""
+   
 def liststring(string):
-    return list(string)
-"""
-    
-def liststring(string):
-    liste = []
-    for i in string:
-        liste.append(i)
-    return list(string)
+    return string.split(' ')  
     
 print(liststring("Okay bro"))
