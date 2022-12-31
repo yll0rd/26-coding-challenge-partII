934dfdf3df3e747fc9bfff7f922593462acab118
Youmbi Leo <Leoyoumbi83@gmail.com>
2022-12-31 15:56:40 +0100

    Add files via upload

---
+++ 50.py
@@ -0,0 +1,12 @@
+from random import randint
+
+def getrandomnumbers(n):
+    ListOfNumbers = []
+    for i in range(0,n):
+        ListOfNumbers.append(randint(1,n))
+        while ListOfNumbers.count(ListOfNumbers[-1]) != 1:
+            ListOfNumbers.pop(-1)
+            ListOfNumbers.append(randint(1,n))
+    return ListOfNumbers
+
+print(getrandomnumbers(6))
